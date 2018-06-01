324a4b2bda091eef4d4f23e22861de4b4af0443a
sasayah <h.s.298355@gmail.com>
2018-06-01 16:10:45 +0900

    add icon borrowButton

--- src/components/MessageBubble/MessageBubble.js
+++ src/components/MessageBubble/MessageBubble.js
@@ -1,28 +1,41 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, Text } from 'react-native'
+import { View, StyleSheet } from 'react-native'
+import { Button, Text, Thumbnail } from 'native-base'
 
 class MessageBubble extends Component {
   render() {
 
     //These spacers make the message bubble stay to the left or the right, depending on who is speaking, even if the message is multiple lines.
-    var leftSpacer = this.props.direction === 'left' ? null : <View style={{width: 70}}/>;
-    var rightSpacer = this.props.direction === 'left' ? <View style={{width: 70}}/> : null;
+    var leftSpacer = this.props.direction === 'left' ? <Thumbnail small source={{uri: "https://facebook.github.io/react-native/docs/assets/favicon.png"}} /> : <View style={{ width: 70 }} />;
+    var rightSpacer = this.props.direction === 'left' ? 
+    (
+      <Button light style={{
+        marginTop: 8,
+        marginRight: 2,
+        marginLeft: 3,
+        height: 30
+      }}>
+        <Text>借りる</Text>
+      </Button>
+    ) 
+    // <View style={{ width: 70 }} />
+    : null;
 
     var bubbleStyles = this.props.direction === 'left' ? [styles.messageBubble, styles.messageBubbleLeft] : [styles.messageBubble, styles.messageBubbleRight];
 
     var bubbleTextStyle = this.props.direction === 'left' ? styles.messageBubbleTextLeft : styles.messageBubbleTextRight;
 
     return (
-        <View style={{justifyContent: 'space-between', flexDirection: 'row'}}>
-            {leftSpacer}
-            <View style={bubbleStyles}>
-              <Text style={bubbleTextStyle}>
-                {this.props.text}
-              </Text>
-            </View>
-            {rightSpacer}
-          </View>
-      );
+      <View style={{ justifyContent: 'space-between', flexDirection: 'row' }}>
+        {leftSpacer}
+        <View style={bubbleStyles}>
+          <Text style={bubbleTextStyle}>
+            {this.props.text}
+          </Text>
+        </View>
+        {rightSpacer}
+      </View>
+    );
   }
 }
 
@@ -30,14 +43,14 @@ const styles = StyleSheet.create({
   //MessageBubble
 
   messageBubble: {
-      borderRadius: 5,
-      marginTop: 8,
-      marginRight: 10,
-      marginLeft: 10,
-      paddingHorizontal: 10,
-      paddingVertical: 5,
-      flexDirection:'row',
-      flex: 1
+    borderRadius: 5,
+    marginTop: 8,
+    marginRight: 10,
+    marginLeft: 10,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    flexDirection: 'row',
+    flex: 1
   },
 
   messageBubbleLeft: {
--- src/screens/BulletinBoard/BulletinBoard.js
+++ src/screens/BulletinBoard/BulletinBoard.js
@@ -1,37 +1,51 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, Text, TextInput,ScrollView } from 'react-native'
+import { View, StyleSheet,  TextInput, ScrollView, Text } from 'react-native'
 import MessageBubble from "../../components/MessageBubble/MessageBubble";
 import InputBar from "../../components/InputBar/InputBar";
 import KeyboardSpacer from 'react-native-keyboard-spacer';
 import MultiSwitch from '../../components/MultiSwitch/MultiSwitch'
 
-
 class BulletinBoardScreen extends Component {
+
+  state = {
+    inputBarText: ''
+  }
+
+
+  _onChangeInputBarText(text) {
+    this.setState({
+      inputBarText: text
+    });
+  }
+
   render() {
     return (
       <View style={styles.outer}>
-      <ScrollView>
-        {}
-      </ScrollView>
-      <View style={styles.multiSwitchContainer}>
-      <MultiSwitch ref={ref => (this.multiSwitch = ref)}/>
+        <ScrollView>
+        <MessageBubble direction='left' text='hello'/>
+        <MessageBubble direction='left' text='hello'/>
+        <MessageBubble direction='right' text='hello'/>
+        <MessageBubble direction='left' text='helloooooooooooooooooooooooooooooooooooooo'/>
+        </ScrollView>
+        <View style={styles.multiSwitchContainer}>
+          <MultiSwitch ref={ref => (this.multiSwitch = ref)} />
+        </View>
+        <InputBar onSendPressed={() => { console.log(this.multiSwitch.state.selectedPosition) }}
+          onSizeChange={() => { }}
+          onChangeText={(text) => { this._onChangeInputBarText(text) }}
+          text={this.state.inputBarText}
+        />
+        <KeyboardSpacer />
       </View>
-      <InputBar onSendPressed={() => {console.log(this.multiSwitch.state.selectedPosition)}} 
-                onSizeChange={() => {}}
-                onChangeText={(text) => {}}
-                text='hrlo'
-                />
-      <KeyboardSpacer/>             
-  </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
 
-  multiSwitchContainer:{
-        alignItems: 'center',
-        justifyContent: 'center',
+  multiSwitchContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 
   //ChatView
@@ -45,63 +59,6 @@ const styles = StyleSheet.create({
 
   messages: {
     flex: 1
-  },
-
-  //InputBar
-
-  inputBar: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    paddingHorizontal: 5,
-    paddingVertical: 3,
-  },
-
-  textBox: {
-    borderRadius: 5,
-    borderWidth: 1,
-    borderColor: 'gray',
-    flex: 1,
-    fontSize: 16,
-    paddingHorizontal: 10
-  },
-
-  sendButton: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingLeft: 15,
-    marginLeft: 5,
-    paddingRight: 15,
-    borderRadius: 5,
-    backgroundColor: '#66db30'
-  },
-
-  //MessageBubble
-
-  messageBubble: {
-    borderRadius: 5,
-    marginTop: 8,
-    marginRight: 10,
-    marginLeft: 10,
-    paddingHorizontal: 10,
-    paddingVertical: 5,
-    flexDirection: 'row',
-    flex: 1
-  },
-
-  messageBubbleLeft: {
-    backgroundColor: '#d5d8d4',
-  },
-
-  messageBubbleTextLeft: {
-    color: 'black'
-  },
-
-  messageBubbleRight: {
-    backgroundColor: '#66db30'
-  },
-
-  messageBubbleTextRight: {
-    color: 'white'
   },
 })
 

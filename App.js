a60fbb365847de5d717be07fc80b7f3b5d04a948
sasayah <h.s.298355@gmail.com>
2018-06-03 17:50:54 +0900

    AuthScreen without firebase

--- App.js
+++ App.js
@@ -1,16 +1,19 @@
 import {Navigation} from 'react-native-navigation'
 import {Provider} from 'react-redux'
 import BulletinBoardScreen from './src/screens/BulletinBoard/BulletinBoard'
+import AuthSwipeScreen from './src/screens/AuthSwipe/AuthSwipe'
 
 import configureStore from './src/store/configureStore'
 
 const store = configureStore();
 
 Navigation.registerComponent("soySauce.BulletinBoardScreen", () => BulletinBoardScreen, store, Provider);
+Navigation.registerComponent("soySauce.AuthSwipeScreen", () => AuthSwipeScreen, store, Provider);
+
 
 Navigation.startSingleScreenApp({
   screen:{
-    screen: "soySauce.BulletinBoardScreen",
-    title: "掲示板"
+    screen: "soySauce.AuthSwipeScreen",
+    title: "login"
   }
 });

a4d5e6ad46a49ab7ecd7b6145cfc6b748393998b
sasayah <h.s.298355@gmail.com>
2018-06-04 16:54:11 +0900

    add side bar action

--- src/screens/BulletinBoard/BulletinBoard.js
+++ src/screens/BulletinBoard/BulletinBoard.js
@@ -26,6 +26,23 @@ class BulletinBoardScreen extends Component {
     messages: []
   }
 
+
+  constructor(props) {
+    super(props);
+    this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent);
+  }
+
+
+  onNavigatorEvent = event => {
+    if (event.type === "NavBarButtonPress") {
+      if (event.id === "sideDrawerToggle") {
+        this.props.navigator.toggleDrawer({
+          side: "left"
+        });
+      }
+    }
+  };
+
   componentWillMount() {
     this.props.onLoadMessages();
   }
